cae2249bcabc9bbfaa4599839e5a42db27639912
mataszilaitis <matas.zilaitis@gmail.com>
2019-02-15 16:07:46 +0000

    Created OpeningHours structured and started to attempt to add hours to the pub

--- app/src/main/java/com/example/matasolutions/pintindex/PubActivity.java
+++ app/src/main/java/com/example/matasolutions/pintindex/PubActivity.java
@@ -1,54 +1,47 @@
 package com.example.matasolutions.pintindex;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.awt.font.TextAttribute;
-import java.io.Serializable;
+public class PubActivity extends FragmentActivity implements OnMapReadyCallback {
 
-public class PubActivity extends AppCompatActivity implements Serializable {
-
-    Pub pub;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pub);
-
-        setupPub();
-
-        TextView pubLocation = findViewById(R.id.pubLocation);
-        pubLocation.setText(getCoordinatesAsText());
-
-    }
-
-    private void setupPub(){
-
-        pub = new Pub();
-
-        Bundle bundle = getIntent().getParcelableExtra("bundle");
-        pub.coordinates = bundle.getParcelable("coordinates");
-
-        pub.name = (String) getIntent().getSerializableExtra("name");
-
-        setTitle(pub.name);
-
-    }
-
-    private String getCoordinatesAsText(){
-        StringBuilder sb = new StringBuilder();
-        sb.append(String.valueOf(pub.coordinates.latitude));
-        sb.append(" ");
-        sb.append(String.valueOf(pub.coordinates.longitude));
-
-        return sb.toString();
-
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
 
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
