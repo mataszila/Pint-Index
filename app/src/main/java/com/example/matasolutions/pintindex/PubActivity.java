4bb24800e7a0f798d17d55508754b85f0a1345ce
mataszilaitis <matas.zilaitis@gmail.com>
2019-03-12 23:39:54 +0000

    Pub is now setup the right way, except the Ratings class

--- app/src/main/java/com/example/matasolutions/pintindex/PubActivity.java
+++ app/src/main/java/com/example/matasolutions/pintindex/PubActivity.java
@@ -76,21 +76,16 @@ public class PubActivity extends AppCompatActivity implements OnMapReadyCallback
         Bundle bundle = getIntent().getParcelableExtra("bundle");
         pub.coordinates = bundle.getParcelable("coordinates");
         pub.name = (String) getIntent().getSerializableExtra("name");
-        workingHours =  (String) getIntent().getSerializableExtra("workingHours");
-        prices = (String) getIntent().getSerializableExtra("prices");
-        //facilities = (String) getIntent().getSerializableExtra("facilities");
-        facilityArrayList = (ArrayList<Facility>) getIntent().getSerializableExtra("facilitiesList");
 
         ratings = (String) getIntent().getSerializableExtra("ratings");
 
         pub.coordinates = bundle.getParcelable("coordinates");
-        pub.name = (String) getIntent().getSerializableExtra("name");
-        marker;
-        pub.weekOpeningHours = new WeekOpeningHours((ArrayList<SingleOpeningHours>) getIntent().getSerializableExtra("workingHoursList"));
-        prices;
-        facilities;
-        ratings;
+        //marker;
 
+        pub.weekOpeningHours = new WeekOpeningHours((ArrayList<SingleOpeningHours>) getIntent().getSerializableExtra("workingHoursList"));
+        pub.prices = new Prices((ArrayList<Price>) getIntent().getSerializableExtra("pricesList"));
+        pub.facilities = new Facilities((ArrayList<Facility>) getIntent().getSerializableExtra("facilitiesList"));
+        pub.ratings = new Ratings();
 
 
 
