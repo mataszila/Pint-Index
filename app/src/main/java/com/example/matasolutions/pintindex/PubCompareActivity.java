2473c6a4c6be4b5059c32f4e339e3aafe55ac6d4
mataszilaitis <matas.zilaitis@gmail.com>
2019-03-12 16:04:45 +0000

    Redirect to PubCompareActivity

---
+++ app/src/main/java/com/example/matasolutions/pintindex/PubCompareActivity.java
@@ -0,0 +1,14 @@
+package com.example.matasolutions.pintindex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PubCompareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pub_compare);
+    }
+}
