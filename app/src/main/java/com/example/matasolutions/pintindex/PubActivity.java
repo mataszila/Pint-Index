1d5a51359dd76af6fdecdb3f7db0d04d900ca371
mataszilaitis <matas.zilaitis@gmail.com>
2019-03-18 17:47:38 +0000

    Added when the pub closes ar opens at to the toolbar

--- app/src/main/java/com/example/matasolutions/pintindex/PubActivity.java
+++ app/src/main/java/com/example/matasolutions/pintindex/PubActivity.java
@@ -20,7 +20,12 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Set;
 
 public class PubActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -106,22 +111,152 @@ public class PubActivity extends AppCompatActivity implements OnMapReadyCallback
 
         toolbar_card_1 = findViewById(R.id.toolbar_card_1);
         toolbar_text_1 = findViewById(R.id.toolbar_text_1);
-        toolbar_text_1.setText("OPEN NOW");
+
+        toolbar_text_1.setText(SetPubOpeningStatus());
 
     }
 
-    private boolean CheckPubOpen(){
+    
 
-        for(int i=0;i<pub.weekOpeningHours.openingHours.size();i++){
-            
+    private String SetPubOpeningStatus(){
 
+        SingleOpeningHours hoursForToday  = pub.weekOpeningHours.openingHours.get(GetCorrectDayOfWeek()-1);
+
+
+        try {
+            if(isPubOpen(hoursForToday.openingTime,hoursForToday.closingTime ,getHoursMinutesNow() )){
+
+
+                return FormatStatusText("OPEN", "CLOSES", hoursForToday.closingTime);
+            }
+            else{
+                return FormatStatusText("CLOSED", "OPENS", hoursForToday.openingTime);
+            }
+
+
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
 
+        return "STATUS N/A";
 
+    }
+
+    private String FormatStatusText(String status, String action, String actionTime) {
+
+        String ans = status + " (" + action + actionTime + ")";
+
+        return ans;
 
 
 
     }
+
+
+    private int GetCorrectDayOfWeek(){
+
+        Calendar calendar  = Calendar.getInstance();
+        int incorrect = calendar.get(Calendar.DAY_OF_WEEK);
+
+        int ans  = incorrect == 1 ? 7 : incorrect - 1;
+        return ans;
+
+    }
+
+
+    private String getHoursMinutesNow(){
+
+        Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
+
+        int numHours = calendar.get(Calendar.HOUR_OF_DAY);
+        int numMinutes = calendar.get(Calendar.MINUTE);
+
+        String hours = String.valueOf(numHours);
+        String minutes = numMinutes < 10 ? "0" + String.valueOf(numMinutes) : String.valueOf(numMinutes);  // gets hour in 24h format
+
+        return hours + ":" + minutes;
+
+    }
+
+
+    public static boolean isPubOpen(String argStartTime,
+                                               String argEndTime, String argCurrentTime) throws ParseException, ParseException {
+        String reg = "^([0-1][0-9]|2[0-3]):([0-5][0-9])$";
+        //
+        if (argStartTime.matches(reg) && argEndTime.matches(reg)
+                && argCurrentTime.matches(reg)) {
+            boolean valid = false;
+            // Start Time
+            java.util.Date startTime = new SimpleDateFormat("HH:mm")
+                    .parse(argStartTime);
+            Calendar startCalendar = Calendar.getInstance();
+            startCalendar.setTime(startTime);
+
+            // Current Time
+            java.util.Date currentTime = new SimpleDateFormat("HH:mm")
+                    .parse(argCurrentTime);
+            Calendar currentCalendar = Calendar.getInstance();
+            currentCalendar.setTime(currentTime);
+
+            // End Time
+            java.util.Date endTime = new SimpleDateFormat("HH:mm")
+                    .parse(argEndTime);
+            Calendar endCalendar = Calendar.getInstance();
+            endCalendar.setTime(endTime);
+
+            //
+            if (currentTime.compareTo(endTime) < 0) {
+
+                currentCalendar.add(Calendar.DATE, 1);
+                currentTime = currentCalendar.getTime();
+
+            }
+
+            if (startTime.compareTo(endTime) < 0) {
+
+                startCalendar.add(Calendar.DATE, 1);
+                startTime = startCalendar.getTime();
+
+            }
+            //
+            if (currentTime.before(startTime)) {
+
+                System.out.println(" Time is Lesser ");
+
+                valid = false;
+            } else {
+
+                if (currentTime.after(endTime)) {
+                    endCalendar.add(Calendar.DATE, 1);
+                    endTime = endCalendar.getTime();
+
+                }
+
+                System.out.println("Comparing , Start Time /n " + startTime);
+                System.out.println("Comparing , End Time /n " + endTime);
+                System.out
+                        .println("Comparing , Current Time /n " + currentTime);
+
+                if (currentTime.before(endTime)) {
+                    System.out.println("RESULT, Time lies b/w");
+                    valid = true;
+                } else {
+                    valid = false;
+                    System.out.println("RESULT, Time does not lies b/w");
+                }
+
+            }
+            return valid;
+
+        } else {
+            throw new IllegalArgumentException(
+                    "Not a valid time, expecting HH:MM:SS format");
+        }
+
+    }
+
+
+
 
 
 
