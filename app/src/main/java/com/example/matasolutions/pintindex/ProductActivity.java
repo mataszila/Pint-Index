61e61cb3583a77256d502420410cb3c811292926
mataszilaitis <matas.zilaitis@gmail.com>
2019-03-23 11:13:27 +0000

    Created three Spinners for selecting a product

--- app/src/main/java/com/example/matasolutions/pintindex/ProductActivity.java
+++ app/src/main/java/com/example/matasolutions/pintindex/ProductActivity.java
@@ -3,7 +3,12 @@ package com.example.matasolutions.pintindex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.EnumSet;
@@ -13,6 +18,15 @@ public class ProductActivity extends AppCompatActivity {
     Spinner DrinkTypeSpinner;
     Spinner BrandSpinner;
     Spinner AmountSpinner;
+
+    DrinkType drinkType;
+    Brand brand;
+    Amount amount;
+
+    TextView textView;
+
+
+    Button actionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +38,92 @@ public class ProductActivity extends AppCompatActivity {
         AmountSpinner =findViewById(R.id.Amount_Spinner);
 
         ArrayList<DrinkType> drinkTypeList = new ArrayList<DrinkType>(EnumSet.allOf(DrinkType.class));
-        ArrayList<Brand> drinkTypeList = new ArrayList<DrinkType>(EnumSet.allOf(DrinkType.class));
-        ArrayList<DrinkType> drinkTypeList = new ArrayList<DrinkType>(EnumSet.allOf(DrinkType.class));
+        ArrayList<Brand> brandList = new ArrayList<Brand>(EnumSet.allOf(Brand.class));
+        ArrayList<Amount> amountList = new ArrayList<Amount>(EnumSet.allOf(Amount.class));
+
+        final ArrayAdapter<DrinkType> drinkTypeAdapter = new ArrayAdapter<DrinkType>(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,drinkTypeList);
+        drinkTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<Brand> brandAdapter = new ArrayAdapter<Brand>(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,brandList);
+        brandAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        final ArrayAdapter<Amount> amountAdapter = new ArrayAdapter<Amount>(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,amountList);
+        brandAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        DrinkTypeSpinner.setAdapter(drinkTypeAdapter);
+        BrandSpinner.setAdapter(brandAdapter);
+        AmountSpinner.setAdapter(amountAdapter);
+
+        DrinkTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+           @Override
+           public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+               drinkType = (DrinkType) adapterView.getItemAtPosition(i);
+           }
+
+           @Override
+           public void onNothingSelected(AdapterView<?> adapterView) {
+
+           }
+       });
+
+
+        BrandSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                brand = (Brand) adapterView.getItemAtPosition(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        AmountSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                amount = (Amount) adapterView.getItemAtPosition(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        actionButton = findViewById(R.id.action_button);
+
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                textView = findViewById(R.id.textview);
+                textView.setText(drinkType.toString() + brand.toString() + amount.toString() );
+
+            }
+        });
+
+
+        Product prod = new Product(brand, drinkType, amount);
+
+        PubSetup setup = new PubSetup();
 
 
 
+        ArrayList<Pub> pubsWithMatchingProduct = new ArrayList<Pub>();
 
 
 
     }
+
+    private void Find
+
+
 }
