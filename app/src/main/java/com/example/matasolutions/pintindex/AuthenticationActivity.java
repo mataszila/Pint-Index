7a2d1a917170cbd1debadb6ff5c7078819380c33
mataszilaitis <matas.zilaitis@gmail.com>
2019-04-08 21:25:08 +0100

    Setup links to other activities and created methods for handling login and registration

--- app/src/main/java/com/example/matasolutions/pintindex/AuthenticationActivity.java
+++ app/src/main/java/com/example/matasolutions/pintindex/AuthenticationActivity.java
@@ -1,12 +1,24 @@
 package com.example.matasolutions.pintindex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AuthenticationActivity extends AppCompatActivity {
+
+
     private FirebaseAuth mAuth;
     private EditText password;
     private EditText email;
@@ -25,5 +37,82 @@ public class AuthenticationActivity extends AppCompatActivity {
             button_login = (Button)findViewById(R.id.button_login);
             mAuth = FirebaseAuth.getInstance();
 
+            button_register.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    if(view == button_register){
+
+                        RegisterUser();
+
+                    }
+                }
+            });
+
+            button_login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    if(view == button_login){
+
+                        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+
+                    }
+
+                }
+            });
+
+
+
+
+
+
+
         }
+
+
+
+
+
+    public void RegisterUser(){
+        String Email = email.getText().toString().trim();
+        String Password = password.getText().toString().trim();
+        if (TextUtils.isEmpty(Email)){
+            Toast.makeText(this, "A Field is Empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(Password)){
+            Toast.makeText(this, "A Field is Empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(Email, Password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        try {
+                            //check if successful
+                            if (task.isSuccessful()) {
+                                //User is successfully registered and logged in
+                                //start Profile Activity here
+                                Toast.makeText(AuthenticationActivity.this, "registration successful",
+                                        Toast.LENGTH_SHORT).show();
+                                finish();
+                                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                            }else{
+                                Toast.makeText(AuthenticationActivity.this, "Couldn't register, try again",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                });
+    }
+
+
+
+
+
+
+
 }
