866668e94ec9dedafcbb621c37e4ac5482540799
Your Name <you@example.com>
2019-04-21 20:52:29 +0100

    Finally fixed elements of PubPageContent

---
+++ app/src/main/java/com/example/matasolutions/pintindex/MyRatingsAdapter.java
@@ -0,0 +1,75 @@
+package com.example.matasolutions.pintindex;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyRatingsAdapter extends RecyclerView.Adapter<MyRatingsAdapter.MyRatingsViewHolder> {
+    private ArrayList<Rating> mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyRatingsViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+
+        public TextView category;
+        public TextView rating;
+
+
+        public MyRatingsViewHolder(View v) {
+            super(v);
+
+            category =  v.findViewById(R.id.pub_activity_ratings_category);
+            rating =  v.findViewById(R.id.pub_activity_ratings_rating);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyRatingsAdapter(ArrayList<Rating> myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyRatingsAdapter.MyRatingsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View v = inflater.inflate(R.layout.pub_activity_ratings_recycler_view, parent, false);
+
+        MyRatingsAdapter.MyRatingsViewHolder vh = new MyRatingsAdapter.MyRatingsViewHolder(v);
+        return vh;
+    }
+
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyRatingsAdapter.MyRatingsViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        Rating thisRating = mDataset.get(position);
+
+        // Set item views based on your views and data model
+        TextView category = holder.category;
+        TextView rating  = holder.rating;
+
+        category.setText(String.valueOf(thisRating.type));
+        rating.setText(String.valueOf(thisRating.averageRating));
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
--- app/src/main/java/com/example/matasolutions/pintindex/PageChildViewHolder.java
+++ app/src/main/java/com/example/matasolutions/pintindex/PageChildViewHolder.java
@@ -20,16 +20,32 @@ public class PageChildViewHolder extends ChildViewHolder {
     private ImageView mImageView;
 
     private LinearLayout pricesLinearLayout;
+    private LinearLayout facilitiesLinearLayout;
+    private LinearLayout ratingsLinearLayout;
+    private LinearLayout openinghoursLinearLayout;
+
+
 
     private RecyclerView prices_recyclerView;
     private RecyclerView.Adapter prices_mAdapter;
     private RecyclerView.LayoutManager prices_layoutManager;
 
-    private LinearLayout facilitiesLinearLayout;
 
     private RecyclerView facilities_recyclerView;
     private RecyclerView.Adapter facilities_mAdapter;
     private RecyclerView.LayoutManager facilities_layoutManager;
+
+
+    private RecyclerView ratings_recyclerView;
+    private RecyclerView.Adapter ratings_mAdapter;
+    private RecyclerView.LayoutManager ratings_layoutManager;
+
+
+    private RecyclerView openinghours_recyclerView;
+    private RecyclerView.Adapter openinghours_mAdapter;
+    private RecyclerView.LayoutManager openinghours_layoutManager;
+
+
 
 
     public PageChildViewHolder(View itemView) {
@@ -42,38 +58,94 @@ public class PageChildViewHolder extends ChildViewHolder {
         mTextView.setText(child.name);
         mImageView = itemView.findViewById(R.id.facilityLogo);
 
+        pricesLinearLayout = itemView.findViewById(R.id.child_prices);
+        facilitiesLinearLayout = itemView.findViewById(R.id.child_facilities);
+        ratingsLinearLayout = itemView.findViewById(R.id.child_ratings);
+        openinghoursLinearLayout = itemView.findViewById(R.id.child_openinghours);
 
-        if (child.type == PubPageCategory.PRICES) {
+        switch (child.type){
 
-            pricesLinearLayout = itemView.findViewById(R.id.child_prices);
-            pricesLinearLayout.setVisibility(View.VISIBLE);
+            case PRICES:
 
-            prices_recyclerView = itemView.findViewById(R.id.child_prices_recyclerView);
+                facilitiesLinearLayout.setVisibility(View.GONE);
+                ratingsLinearLayout.setVisibility(View.GONE);
+                openinghoursLinearLayout.setVisibility(View.GONE);
 
-            prices_recyclerView.setHasFixedSize(true);
 
-            prices_layoutManager = new LinearLayoutManager(itemView.getContext());
-            prices_recyclerView.setLayoutManager(prices_layoutManager);
+                pricesLinearLayout.setVisibility(View.VISIBLE);
 
-            prices_mAdapter = new MyPricesAdapter(child.pub.prices.priceList);
-            prices_recyclerView.setAdapter(prices_mAdapter);
+                prices_recyclerView = itemView.findViewById(R.id.child_prices_recyclerView);
 
-        } else if (child.type == PubPageCategory.FACILITIES) {
+                prices_recyclerView.setHasFixedSize(true);
 
-            facilitiesLinearLayout = itemView.findViewById(R.id.child_facilities);
-            facilitiesLinearLayout.setVisibility(View.VISIBLE);
+                prices_layoutManager = new LinearLayoutManager(itemView.getContext());
+                prices_recyclerView.setLayoutManager(prices_layoutManager);
 
-            facilities_recyclerView = itemView.findViewById(R.id.child_facilities_recyclerView);
+                prices_mAdapter = new MyPricesAdapter(child.pub.prices.priceList);
+                prices_recyclerView.setAdapter(prices_mAdapter);
 
-            facilities_recyclerView.setHasFixedSize(true);
 
-            facilities_layoutManager = new LinearLayoutManager(itemView.getContext());
-            facilities_recyclerView.setLayoutManager(facilities_layoutManager);
+                break;
+            case FACILITIES:
 
-            facilities_mAdapter = new MyFacilitiesAdapter(child.pub.facilities.facilities);
-            facilities_recyclerView.setAdapter(facilities_mAdapter);
+                pricesLinearLayout.setVisibility(View.GONE);
+                ratingsLinearLayout.setVisibility(View.GONE);
+                openinghoursLinearLayout.setVisibility(View.GONE);
 
+
+                facilitiesLinearLayout.setVisibility(View.VISIBLE);
+
+                facilities_recyclerView = itemView.findViewById(R.id.child_facilities_recyclerView);
+
+                facilities_recyclerView.setHasFixedSize(true);
+
+                facilities_layoutManager = new LinearLayoutManager(itemView.getContext());
+                facilities_recyclerView.setLayoutManager(facilities_layoutManager);
+
+                facilities_mAdapter = new MyFacilitiesAdapter(child.pub.facilities.facilities);
+                facilities_recyclerView.setAdapter(facilities_mAdapter);
+
+
+                break;
+            case RATINGS:
+
+                pricesLinearLayout.setVisibility(View.GONE);
+                facilitiesLinearLayout.setVisibility(View.GONE);
+                openinghoursLinearLayout.setVisibility(View.GONE);
+
+                ratingsLinearLayout.setVisibility(View.VISIBLE);
+
+                ratings_recyclerView = itemView.findViewById(R.id.child_ratings_recyclerView);
+
+                ratings_recyclerView.setHasFixedSize(true);
+
+                ratings_layoutManager = new LinearLayoutManager(itemView.getContext());
+                ratings_recyclerView.setLayoutManager(ratings_layoutManager);
+
+                ratings_mAdapter = new MyRatingsAdapter(child.pub.ratings.ratings);
+                ratings_recyclerView.setAdapter(ratings_mAdapter);
+                break;
+            case OPENING_HOURS:
+
+                pricesLinearLayout.setVisibility(View.GONE);
+                facilitiesLinearLayout.setVisibility(View.GONE);
+                ratingsLinearLayout.setVisibility(View.GONE);
+
+                openinghoursLinearLayout.setVisibility(View.VISIBLE);
+
+                openinghours_recyclerView = itemView.findViewById(R.id.child_openinghours_recyclerView);
+
+                openinghours_recyclerView.setHasFixedSize(true);
+
+                openinghours_layoutManager = new LinearLayoutManager(itemView.getContext());
+                openinghours_recyclerView.setLayoutManager(openinghours_layoutManager);
+
+                openinghours_mAdapter = new MyOpeningHoursAdapter(child.pub.weekOpeningHours.openingHours);
+                openinghours_recyclerView.setAdapter(openinghours_mAdapter);
+
+                break;
         }
+
 
     }
 
